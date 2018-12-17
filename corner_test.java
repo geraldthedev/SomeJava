449e7bd2e5519f5aec007f2431b2121d924ae7e9
Gerald Bryant <hakeem1106@gmail.com>
2018-12-17 20:42:39 +0000

    java

---
+++ corner_test.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+import java.lang.*;
+
+
+public class corner_test {
+
+    public int roll;
+    public int bounce;
+    public int hill;
+
+
+}
+
