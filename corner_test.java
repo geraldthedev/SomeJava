8c044c63188e7561ac56db51edc4dd9d42558bf6
Gerald Bryant <hakeem1106@gmail.com>
2018-12-18 14:00:32 +0000

    creating objects

--- corner_test.java
+++ corner_test.java
@@ -4,21 +4,37 @@ import java.math.*;
 import java.lang.*;
 
 
-public class corner_test {
-
-   Scanner roll = new Scanner(System.in);
-   Scanner bounce = new Scanner(System.in);
-   Scanner hill = new Scanner(System.in);
+class corner_test {
+    public static void main(String[] args){
 
 
+   intScanner roll = new Scanner(System.in);
+   intScanner bounce = new Scanner(System.in);
+   intScanner hill = new Scanner(System.in);
 
-    public void rollHill(int height, int angle, int drop){
+   System.out.println("Enter a roll number:");
+    int newRoll = sc.nextInt();
+    System.out.println("Enter a bounce number:");
+    int newBounce = sc.nextInt();
+    System.out.println("Enter a hill number:");
+    int newHill = sc.nextInt();
+
+    roll = newRoll;
+    bounce = newBounce;
+    hill = newHill;
+}
+
+ public void rollHill(int height, int angle, int drop){
         height = 0;
         angle = roll * hill;
+        drop = angle/roll+4;
+
+    if(roll ){
+
+    }
+
 }
 
-
 }
-
 
 
