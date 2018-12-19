65302a0c8ebe86e0e8b12e0d849be29b5203a120
Gerald Bryant <hakeem1106@gmail.com>
2018-12-19 14:57:04 +0000

    updating

---
+++ corner_test/src/main/java/com/corner_test/app/App.java
@@ -0,0 +1,41 @@
+package com.corner_test.app;
+import java.util.*;
+import java.io.*;
+import java.math.*;
+import java.lang.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        intScanner roll = new Scanner(System.in);
+   intScanner bounce = new Scanner(System.in);
+   intScanner hill = new Scanner(System.in);
+
+   System.out.println("Enter a roll number:");
+    int newRoll = sc.nextInt();
+    System.out.println("Enter a bounce number:");
+    int newBounce = sc.nextInt();
+    System.out.println("Enter a hill number:");
+    int newHill = sc.nextInt();
+
+    roll = newRoll;
+    bounce = newBounce;
+    hill = newHill;
+    }
+
+    public void rollHill(int height, int angle, int drop){
+        height = 0;
+        angle = roll * hill;
+        drop = angle/roll+4;
+
+    if(roll ){
+
+    }
+
+}
+}
